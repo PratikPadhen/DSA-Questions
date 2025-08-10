1de0f2889ce4b52fb2069b0055077f02c1a7c02e
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-08-10 13:51:49 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (62.73%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>count; /// mapping which is unordered
-        int half=nums.size()/2;
-        for(int it : nums ){
-            count[it]+=1;
-        }
-        for(auto &it1 : count){   ///using this & in for each loop in seen of unorderred mapping 
-            if(it1.second > half ){//is extreamly good as it saves place....BEST PRACTICE
-                return it1.first;
+    int majorityElement(vector<int>& v) {
+        int count=0;
+        int ele;
+        for(int i=0;i<v.size();i++){
+            if(count==0){
+                ele=v[i];
+                count++;
             }
+            else if(v[i]==ele){
+                count++;
+            }
+            else{
+                count--;
+            }
+            
         }
+        
+        for(int i=0;i<v.size();i++){
+            if(v[i]==ele) count++;
+        }
+        if(count > (v.size() / 2)){
+           return ele;
+        }
+        return -1;
+    //     // solving by the Mapping ->
+    //     unordered_map<int,int>count;
+    //     int half=nums.size()/2;
+    //     for(int it : nums ){
+    //         count[i]+=1;
+    //     }
+    //     for(int &it1 : count){
+    //         if(it1.second > half ){
+    //             return it1.first;
+    //         }
+    //     }
 
-      return -1;
+    //   return -1;
         
     }
 };
