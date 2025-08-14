253c064632c07e87d8be2f9d1e5ecdc8204c3241
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-08-14 09:59:02 +0530

    Time: 12 ms (26.43%) | Memory: 145.1 MB (9.53%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -14,3 +14,30 @@ public:
         return ans;
     }
 };
+// what if pos > neg or neg > pos or vicevers of it
+/// Code for such cases...->
+//initally creating two seprate vectors storing pos and negative ele
+//if(pos.size() > neg.size()){
+//     for(int i=0; i<neg.size();i++){
+//         ans[2*i]=pos[i];
+//         ans[2*i+1]=neg[i];
+//     }
+//     int index=neg.size()*2;
+//     for(int i=neg.size();i<pos.size();i++){
+//         ans[index]=pos[i];
+//         index++;
+//     }
+// }
+// else{
+//     for(int i=0; i<pos.size();i++){
+//         ans[2*i]=neg[i];
+//         ans[2*i+1]=pos[i];
+//     }
+//     int index=pos.size()*2;
+//     for(int i=pos.size();i<neg.size();i++){
+//         ans[index]=neg[i];
+//         index++;
+//     }
+
+// }
+// return ans;
