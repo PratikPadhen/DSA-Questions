a6e9335d6197a3b0784d7564b0c89b3178b30b81
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-08-17 19:39:43 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (92.21%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
+//   TRANPOSE + REVERSE TECHNIQUE
+
     void rotate(vector<vector<int>>& matrix) {
          int n=matrix.size();
         if(n<=0) return ;
-        for( int i=0 ; i < n; i++){
+        for( int i=0 ; i < n; i++){  ///here it is taking tranpose of matrix
             for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for( int i=0;i<n;i++){
+        for( int i=0;i<n;i++){           /// simply reverse it..
             reverse(matrix[i].begin(),matrix[i].end());
         }   
     }
