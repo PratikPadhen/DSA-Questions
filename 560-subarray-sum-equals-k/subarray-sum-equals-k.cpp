ae10967047b810089c1ff1abeab8ea8404f33fa3
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-08-18 00:49:07 +0530

    Time: 41 ms (69.50%) | Memory: 45.5 MB (56.22%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k){
-        // most optimal solution ->
-        int presum=0;
-        int count=0;
-        unordered_map<int,int>map;
-        map[0]=1;  
-        for(int i=0;i<nums.size();i++){
-            presum+=nums[i];
-            int remove=presum-k;
-            count+=map[remove];
-            map[presum]+=1;
+    int subarraySum(vector<int>& nums, int k) {
+        // Running prefix sum of elements seen so far
+        int presum = 0;
+        // Total count of subarrays whose sum equals k
+        int count = 0;
+        // Hash map: prefix sum → frequency of occurrences
+        // Initialize with sum 0 seen once (empty prefix)
+        unordered_map<int,int> map;
+        map[0] = 1;
+
+        // Iterate through the array, updating prefix sum
+        for (int i = 0; i < nums.size(); i++) {
+            presum += nums[i];               // Extend running sum
+
+            int remove = presum - k;         // Desired previous sum
+            // If a prefix with sum = remove exists, 
+            // then subarray from right after that prefix to current index sums to k
+            if (map.count(remove)) {
+                count += map[remove];        // Add all such occurrences
+            }
+
+            // Record current prefix sum in map
+            map[presum] += 1;
         }
+
+        return count;
+    
+
         return count;
     /// this is bette solution-> 
         // int subarraySum(vector<int>& nums, int k) {
