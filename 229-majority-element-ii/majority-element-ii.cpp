acd3f158033e73fe22cee5b2849a698c1c1c5638
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-09-04 14:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (47.36%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-             int n=nums.size();
-         vector<int>ans;
-        unordered_map<int,int>map;
-        for(auto it : nums ){
-            map[it]+=1;
-        }
-        for(auto p:map){
-            if(p.second > n/3){
-                ans.push_back(p.first);
+        int n=nums.size();
+        int count1=0, count2=0;
+        int ele1,ele2;
+        for(int i=0;i<n;i++){
+            if(count1==0 && (count2==0 || ele2!=nums[i])){
+                count1=1;
+                ele1=nums[i];
+            }
+            else if(count2==0 && ele1!=nums[i]){
+                count2=1;
+                ele2=nums[i];
+            }
+            else if(nums[i]==ele1) count1++;
+            else if(nums[i]==ele2) count2++;
+
+
+            else{
+                count1--,count2--;
             }
         }
+        vector<int>ans;
+         count1=0,count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==ele1) count1++;
+            if(nums[i]==ele2) count2++;
+        }
+        if(count1 > n/3) ans.push_back(ele1);
+        if((count2 > n/3) && count2 > 0 && ele1!=ele2 ) ans.push_back(ele2);
+
+
+        sort(ans.begin(),ans.end());
+
 
         return ans;
     }
