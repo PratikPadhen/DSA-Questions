835a06920d04f5c7c6fe719fe7b72531632bece4
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-09-09 17:13:58 +0530

    Time: 4 ms (22.19%) | Memory: 17.7 MB (63.07%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -2,27 +2,40 @@ class Solution {
 public:
     string largestOddNumber(string num) {
 
-        //// THIS LOGIC IS THOUGHT BY U ->
-          // check if whole num ends with odd digit; if yes, return num
-          if (!num.empty() && ((num.back() - '0') % 2 != 0)) {
-            return num;
-          }
-          else{
-            // not needed for this problem, but keeping your format and using a simple scan
-            // to find the rightmost odd digit and return the prefix up to it
-            int maxodd = -1;
-            for (int i = (int)num.size() - 1; i >= 0; --i) {
-                if (((num[i] - '0') % 2) != 0) { // odd digit found
-                    maxodd = i;
-                    break;
-                }
+        /// simply just trace from the back of string 
+        
+        for (int i = (int)num.size() - 1; i >= 0; --i) {
+            if ((num[i] - '0') % 2 == 1) {
+                return num.substr(0, i + 1);
             }
-            if (maxodd == -1) return ""; // no odd digit
+        }
+        return "";
+    
 
-            return num.substr(0, maxodd + 1);
-          }
+
+        // //// THIS LOGIC IS THOUGHT BY U ->
+        // /// core thing is that start finding for odd from last ...Always
+        // /// if last digit is odd then that number becomes Odd..
+        // /// then return string from 0 ie first digit ->upto last odd digit ie substr;
+        //   // check if whole num ends with odd digit; if yes, return num
+        //   if (!num.empty() && ((num.back() - '0') % 2 != 0)) {
+        //     return num;
+        //   }
+        //   else{
+        //     // not needed for this problem, but keeping your format and using a simple scan
+        //     // to find the rightmost odd digit and return the prefix up to it
+        //     int maxodd = -1;
+        //     for (int i = (int)num.size() - 1; i >= 0; --i) {
+        //         if (((num[i] - '0') % 2) != 0) { // odd digit found
+        //             maxodd = i;
+        //             break;
+        //         }
+        //     }
+        //     if (maxodd == -1) return ""; // no odd digit
+
+        //     return num.substr(0, maxodd + 1);
+        //   }
           // unreachable, but preserved format
           // return ans;  // removed: ans was not defined in this scope
     }
 };
-;
