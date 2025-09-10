6b02ce80f294888bfc1e17c22f9542371cd580dc
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-09-10 19:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (99.66%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+     string ans="";
+     char ch;
+     for(int i=0;i<strs[0].length();i++){
+         ch=strs[0][i];
+     
+      bool match=true;
+      for(int j=1;j<strs.size();j++){
+        if(strs[j].length() <= i || ch!=strs[j][i] ){
+            match=false;
+            break;
+        }
+        
+      }
+     
+      if(match==false){
+        break;
+      }
+      else{
+        ans.push_back(ch);
+      }
+
+     }
+      return ans;
+        
+    }
+};
