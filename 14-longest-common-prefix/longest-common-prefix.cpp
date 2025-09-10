a0f557ddf473da075e2e5de92acc5e897900a00e
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-09-10 19:23:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (84.81%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
+    // Function to find the longest common prefix among all strings in the vector
     string longestCommonPrefix(vector<string>& strs) {
-     string ans="";
-     char ch;
-     for(int i=0;i<strs[0].length();i++){
-         ch=strs[0][i];
-     
-      bool match=true;
-      for(int j=1;j<strs.size();j++){
-        if(strs[j].length() <= i || ch!=strs[j][i] ){
-            match=false;
-            break;
-        }
-        
-      }
-     
-      if(match==false){
-        break;
-      }
-      else{
-        ans.push_back(ch);
-      }
+        string ans = "";                  // Will hold the common prefix
+        char ch;                          // To store current character being checked
 
-     }
-      return ans;
-        
+        // Loop over each character index i of the first string
+        for (int i = 0; i < strs[0].length(); i++) {
+            ch = strs[0][i];             // Take the i-th character from the first string
+
+            bool match = true;           // Assume all strings match at position i
+
+            // Compare this character with the i-th character of every other string
+            for (int j = 1; j < strs.size(); j++) {
+                // If the j-th string is too short (length <= i)
+                // or its i-th character differs from ch
+                if (strs[j].length() <= i || strs[j][i] != ch) {
+                    match = false;       // Mismatch found
+                    break;               // Stop checking further strings
+                }
+            }
+
+            // If any string failed to match at position i, break out
+            if (!match) {
+                break;
+            } 
+            // Otherwise, all strings had ch at position i, so append it
+            else {
+                ans.push_back(ch);
+            }
+        }
+
+        return ans;                     // Return the built prefix
     }
 };
