e599749fb9597909a46e22341547612e18eb8b8b
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-09-10 22:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.73%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()) return false;
+        for(int i=0;i<s.size();i++){
+            if(s==goal){
+                return true;
+            }
+            rotate(s.begin(),s.begin()+1,s.end());
+        }
+        return false;
+    }
+};
