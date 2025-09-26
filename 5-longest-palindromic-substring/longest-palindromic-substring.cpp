a877df688ee06dbbe8a2725e2ee19d8dd0395af2
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-09-26 13:52:00 +0530

    Time: 1788 ms (5.00%) | Memory: 491.6 MB (5.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    bool ispalindorme(string &s,int start,int end){
-        while(start<end){
-            if(s[start]!=s[end]){
+    // Helper function: checks if s[start…end] is a palindrome
+    bool ispalindorme(string &s, int start, int end) {
+        // Compare mirrored characters moving inward
+        while (start < end) {
+            if (s[start] != s[end])   // Mismatch ⇒ not a palindrome
                 return false;
-            }
-            start++,end--;
-            
+            start++;
+            end--;
         }
-        return true;
+        return true;                  // All pairs matched ⇒ palindrome
     }
+
     string longestPalindrome(string s) {
-        string ans="";
-        for(int i=0;i<s.size();i++){
-            for(int j=i;j<s.size();j++){
-                if(ispalindorme(s,i,j)){
-                    string t=s.substr(i,j-i+1);
-                    if(t.size() > ans.size()){
-                        ans=t;
+        string ans = "";              // Will store the longest palindrome found
+        
+        int n = s.size();
+        // 1. Enumerate every possible substring by its start index i…
+        for (int i = 0; i < n; i++) {
+            // 2. …and end index j (j ≥ i)
+            for (int j = i; j < n; j++) {
+                // 3. If s[i…j] is a palindrome…
+                if (ispalindorme(s, i, j)) {
+                    // 4. Extract the substring
+                    string t = s.substr(i, j - i + 1);
+                    // 5. Update ans if this palindrome is longer
+                    if (t.size() > ans.size()) {
+                        ans = t;
                     }
                 }
             }
         }
-        return ans;
+        
+        return ans;                   // Return the longest palindromic substring
     }
 };
