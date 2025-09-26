5522561ba4af68ea976ff5e561e250753723bfc2
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-09-26 13:41:02 +0530

    Time: 1844 ms (5.00%) | Memory: 491.8 MB (5.00%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool ispalindorme(string &s,int start,int end){
+        while(start<end){
+            if(s[start]!=s[end]){
+                return false;
+            }
+            start++,end--;
+            
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            for(int j=i;j<s.size();j++){
+                if(ispalindorme(s,i,j)){
+                    string t=s.substr(i,j-i+1);
+                    if(t.size() > ans.size()){
+                        ans=t;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
