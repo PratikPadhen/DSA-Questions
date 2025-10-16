1e9bb693bdf33ffafda0ff5d54b127c9f8d34656
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-10-16 16:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (91.99%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string str = to_string(x);
-        string rev=str;
-        reverse(rev.begin(),rev.end());
-         if(rev==str){
+        if(x < 0){
+            return false;
+        }
+
+        int revdigit=0;
+        int n=x;
+        while(n > 0){
+            int digit = n % 10;
+
+        
+
+            if(revdigit > (INT_MAX-digit)/10){
+                return false;
+            }
+            revdigit=revdigit*10+digit;
+
+            n=n/10;
+        }
+
+        if(x==revdigit){
             return true;
-         }
-         else{
-            return  false;
-         }
+        }
+        else{
+            return false;
+        }
+       // It is done after converting int -> string  ;
+        // string str = to_string(x);
+        // string rev=str;
+        // reverse(rev.begin(),rev.end());
+        //  if(rev==str){
+        //     return true;
+        //  }
+        //  else{
+        //     return  false;
+        //  }
     }
 };
