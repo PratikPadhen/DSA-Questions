76f8908927b4da7cfc5d71609c5f517c62397780
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-10-24 21:19:17 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (70.01%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        if(nums.size() == 0) return 0;
+        int n=nums.size();
+        int sum1=(n*(n+1))/2;
+        int sum2=0;
 
-        sort(nums.begin(), nums.end());
-
-        for(int i = 0; i < nums.size() - 1; i++) {
-            if(nums[i + 1] != nums[i] + 1) {
-                return nums[i] + 1;
-            }
+        for(int i=0;i<n;i++){
+           sum2+=nums[i];
         }
 
-        // If all numbers from 0 to n-1 are present, then missing is n
-        if(nums[0] != 0) return 0;
-        return nums[nums.size() - 1] + 1;
+        return sum1-sum2;
+        // if(nums.size() == 0) return 0;
+
+        // sort(nums.begin(), nums.end());
+
+        // for(int i = 0; i < nums.size() - 1; i++) {
+        //     if(nums[i + 1] != nums[i] + 1) {
+        //         return nums[i] + 1;
+        //     }
+        // }
+
+        // // If all numbers from 0 to n-1 are present, then missing is n
+        // if(nums[0] != 0) return 0;
+        // return nums[nums.size() - 1] + 1;
     }
 };
