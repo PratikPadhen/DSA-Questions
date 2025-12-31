07f79389f1f4582ed945551e026e55e4c3faee68
Prateek Chandramani Padhen <pratikpadhen@gmail.com>
2025-12-31 14:44:52 +0530

    Time: 0 ms (100.00%) | Memory: 61.5 MB (31.13%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -3,37 +3,37 @@ class Solution {
         int n = nums.length;
         k = k % n;
 
-        List<Integer>list= new ArrayList<>();
+        // List<Integer>list= new ArrayList<>();
 
-        for(int num:nums){
-            list.add(num);
-        }
+        // for(int num:nums){
+        //     list.add(num);
+        // }
 
-        Collections.reverse(list);
-        Collections.reverse(list.subList(0,k));
-        Collections.reverse(list.subList(k,n));
+        // Collections.reverse(list);
+        // Collections.reverse(list.subList(0,k));
+        // Collections.reverse(list.subList(k,n));
 
-        for(int i=0;i<n;i++){
-            nums[i]=list.get(i);
-        }
+        // for(int i=0;i<n;i++){
+        //     nums[i]=list.get(i);
+        // }
 
 
-        // reverse(nums,0,n-1);  It is the part of commented code 
-        // reverse(nums,0,k-1);
-        // reverse(nums,k,n-1);
+        reverse(nums,0,n-1);  //It is the part of commented code 
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
         
     }
-    // public void reverse(int[] nums,int left,int right){
-    //   int temp=0;
-    //    while(left < right){
+    public void reverse(int[] nums,int left,int right){
+      int temp=0;
+       while(left < right){
       
-    //       temp=nums[left];
-    //       nums[left]=nums[right];
-    //       nums[right]=temp;
+          temp=nums[left];
+          nums[left]=nums[right];
+          nums[right]=temp;
 
-    //       left++;
-    //       right--;  
-    // }
+          left++;
+          right--;  
+    }
 
-    // }
+    }
 }
